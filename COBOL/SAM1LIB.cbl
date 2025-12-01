ef529d03f13cd82a9dd2e2cae2960365cf709c10
norbertabelredei <Norbert.Abel.Redei@ibm.com>
2025-12-01 10:02:16 +0100

    test2

--- COBOL/SAM1LIB.cbl
+++ COBOL/SAM1LIB.cbl
@@ -6,7 +6,7 @@
       * OR DISCLOSURE RESTRICTED BY GSA ADP SCHEDULE
       * CONTRACT WITH IBM CORPORATION
       ****************************************************************
-      * PROGRAM:  SAM1B
+      * PROGRAM:  SAM1C
       *
       * AUTHOR :  Doug Stout
       *
